dc8030a060cd143818c35ea6d629367525afcb81
Russ Zurba <r.zurba@speedandfunction.com>
2016-01-18 12:12:54 -0500

    Moving reset password functionality

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -43,4 +43,44 @@ angular.module('PoliciesApp')
         $rootScope.$broadcast('setAlert', 'Please, fill in the form correctly', 'error');
       }
     };
+
+    $scope.resetPassword = function() {
+      if ($scope.form_login.$valid) {
+        wsdl('app', 'RequestResetPassword', {
+          data: {
+            user: {
+              UserName: $scope.login.username,
+              UserPassword: $scope.login.passwordOld
+            }
+          }
+        }).then(function(response){
+          console.log('reset', response);
+          $rootScope.$broadcast('setAlert', 'You password reset request has been submitted successfully');
+          $rootScope.user = {
+            UserName: $scope.login.username,
+            UserPassword: $scope.login.passwordOld
+          };
+          $cookies.put('login', $scope.login.username);
+          $cookies.put('pass', $scope.login.passwordOld);
+          $location.path('/home');
+          wsdl('app', 'GetPolicyOwnerPersonalInfo', {
+            data: {
+              user: $rootScope.user
+            }
+          }).then(function(response){
+            $rootScope.owner = response.data.GetPolicyOwnerPersonalInfoResult;
+            console.log('owner', $rootScope.owner);
+          });
+            // if (response.data && response.data.IsUserAuthenticatedResult) {
+            //   $rootScope.$broadcast('setAlert', response.data.IsUserAuthenticatedResult, 'error');
+            // } else {
+            //   $rootScope.$broadcast('setAlert', 'The service is temporarly unavailable, pleaes try again later', 'error');
+            // }
+        });
+
+      } else {
+        $rootScope.$broadcast('setAlert', 'Please, fill in the form correctly', 'error');
+      }
+    };
+
   }]);
